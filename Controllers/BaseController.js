a7713d0be37fb15fe1a5305117a94ac79c5ca138
Nick van de Luijtgaarden <rob4107@robeco.nl>
2017-03-08 09:46:21 +0100

    Adds documentation to the Base Controller.

--- Controllers/BaseController.js
+++ Controllers/BaseController.js
@@ -7,85 +7,160 @@ BaseController = function(){}
 
 // methods below need to be overwritten by the subclasses
 BaseController.prototype._init = function(){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._getTransactions = function(){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._getBlocks = function(){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
-BaseController.prototype._getBlock = function(blockNumber){
- throw "NotImplementedException";
+BaseController.prototype._getBlock = function(blockNumber){	
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._getInfo = function(){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
-BaseController.prototype._getBalance = function(accountNumber){
- throw "NotImplementedException";
+BaseController.prototype._getBalance = function(accountNumber){	
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._getTransactionHistory = function(accountNumber){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._sendTransaction = function(from, to, amount){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
 BaseController.prototype._getAccountNumber = function(){
- throw "NotImplementedException";
+	throw "NotImplementedException";
 };
 
 // methods below call the above methods that are overwritten by the subclasses
 BaseController.prototype.init = function(){
- console.log("\nInitialising the controller...");
- this._init();
+	console.log("\nInitialising the controller...");
+	this._init();
 };
 
 BaseController.prototype.getBlocks = function(){
- console.log("\nRetrieving five latest blocks...");
- return(this._getBlocks());
+	/*
+	var transaction = {
+		from: string,
+		to: string,
+		timestamp: timestamp,
+		value: int	
+	};
+	/*
+	var block = { 
+		number: INT, 
+		transactions: list<transaction>, 
+		timestamp: INT
+	};
+	
+	Should Return an array of block objects.
+	*/
+	
+	console.log("\nRetrieving five latest blocks...");
+	return(this._getBlocks());
 };
 
 BaseController.prototype.getBlock = function(blockNumber){
- console.log("\nRetrieving block...");
- return(this._getBlock(blockNumber));
+	/*
+	var transaction = {
+		from: string,
+		to: string,
+		timestamp: timestamp,
+		value: int	
+	};
+	
+	var block = { 
+		number: INT, 
+		transactions: list<transaction>, 
+		timestamp: INT
+	};
+	
+	Returns a block object.
+	*/
+	
+	console.log("\nRetrieving block...");
+	return(this._getBlock(blockNumber));
 };
 
 BaseController.prototype.getTransactions = function(){
- console.log("\nRetrieving five latest transactions...");
- return(this._getTransactions());
+	/*
+	var transaction = {
+		from: string,
+		to: string,
+		timestamp: timestamp,
+		value: int	
+	};
+	
+	Returns an array of transaction objects.
+	*/
+	
+	console.log("\nRetrieving five latest transactions...");
+	return(this._getTransactions());
 };
 
 BaseController.prototype.getInfo = function(){
- console.log("\nRetrieving info...");
- return(this._getInfo());
+	/*
+	var info = {
+		network: string,
+		totalMoney: int,
+		currentBlockNumber: int,
+		currentTime: dateTime,
+	};
+	
+	Returns an info object.
+	*/
+	
+	console.log("\nRetrieving info...");
+	return(this._getInfo());
 };
 
 BaseController.prototype.getBalance = function(accountNumber){
- console.log("\nRetrieving balance...");
- return(this._getBalance(accountNumber));
+	/*
+	Returns a string.
+	*/
+	
+	console.log("\nRetrieving balance...");
+	return(this._getBalance(accountNumber));
 };
 
 BaseController.prototype.getTransactionHistory = function(accountNumber){
- console.log("Retrieving transaction history...");
- return(this._getTransactionHistory(accountNumber));
+	/*
+	var transaction = {
+		to: string,
+		block: int,
+		timestamp: timestamp,
+		amount: int
+	};
+	
+	Returns an array of transaction objects.
+	*/
+	
+	console.log("Retrieving transaction history...");
+	return(this._getTransactionHistory(accountNumber));
 };
 
 BaseController.prototype.sendTransaction = function(from, to, amount){
- console.log("\nSending transaction...");
- return(this._sendTransaction(from, to, amount));
+	console.log("\nSending transaction...");
+	return(this._sendTransaction(from, to, amount));
 };
 
 BaseController.prototype.getAccountNumber = function(){
- console.log("\nRetrieving account number...");
- return(this._getAccountNumber());
+	/*
+	Returns a string.	
+	*/
+	
+	console.log("\nRetrieving account number...");
+	return(this._getAccountNumber());
 };
 
 module.exports = BaseController;
